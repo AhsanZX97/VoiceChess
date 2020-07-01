e1a73a0d3a9bc886db302aa1dec24d087e71a114
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-07-01 18:55:10 +0100

    changed square to a class

--- App/Components/Square.js
+++ App/Components/Square.js
@@ -2,45 +2,59 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
 import { Icon } from 'react-native-elements'
 
-export default function Square(props) {
-    var piece;
-    if (props.style) {
-        piece = (
-            <View style ={{
-                flex: 1,
-                justifyContent: 'flex-end',
-                marginBottom: 2
-            }}>
-            <Icon
-                name={props.style.name}
-                type='font-awesome-5'
-                color={props.style.colour} 
-                size = {30}/>
-            </View>
-        )
+export default class Square extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            piece: undefined
+        }
+        if (this.props.style) {
+            this.setState({
+                piece: (
+                    <View style={{
+                        flex: 1,
+                        justifyContent: 'flex-end',
+                        marginBottom: 2
+                    }}>
+                        <Icon
+                            name={props.style.name}
+                            type='font-awesome-5'
+                            color={props.style.colour}
+                            size={30} />
+                    </View>
+                )
+            })
+        }
     }
-    return (
-        <View>
-            <TouchableOpacity
-                onClick={props.onClick}
-                style={{
-                    backgroundColor: props.shade,
-                    fontSize: 24,
-                    fontWeight: 'bold',
-                    lineHeight: 34,
-                    height: 48,
-                    marginRight: -1,
-                    marginTop: -1,
-                    textAlign: 'left',
-                    width: 48,
-                }}
-            >
-                {piece}
-            </TouchableOpacity>
 
 
-        </View>
-    );
+    render() {
+
+        return (
+            <View>
+                <TouchableOpacity
+                    onClick={props.onClick}
+                    style={{
+                        backgroundColor: props.shade,
+                        fontSize: 24,
+                        fontWeight: 'bold',
+                        lineHeight: 34,
+                        height: 48,
+                        marginRight: -1,
+                        marginTop: -1,
+                        textAlign: 'left',
+                        width: 48,
+                    }}
+                >
+                    {this.state.piece}
+                </TouchableOpacity>
+
+
+            </View>
+        );
+    }
 }
 /*
         backgroundColor: 'rgba(52, 52, 52, 0.8)',
